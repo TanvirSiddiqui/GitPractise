7a145ef01f0c28cdc3bbaa5f9cf3bccb00a6dd55
TanvirSiddiqui <babatanvir@hotmail.com>
2020-03-01 13:25:13 +0600

    practing branching

---
+++ reverseString/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace reverseString
+{
+    class Program
+    {
+        public void stringReverse(string str)
+        {
+            char[] chars = str.ToCharArray();
+             for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                char c = chars[i];
+                chars[i] = chars[j];
+                chars[j] = c;
+            }
+            Console.WriteLine(chars);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int no_of_input=0;
+
+            /*  string reversedString= p.stringReverse("Joy Bangla");
+
+               Console.WriteLine(reversedString);*/
+
+            no_of_input = int.Parse(Console.ReadLine());
+
+            string[] word = new string[no_of_input];
+
+            for(int i=0; i < no_of_input; i++)
+            {
+                word[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < no_of_input; i++)
+            {
+                p.stringReverse(word[i]);
+            }
+
+
+
+        }
+    }
+}
